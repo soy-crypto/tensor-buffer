d687790e1b6ea6ac1bdcc288c49465fa976c517b
peterzhu <ztj_nwu@snnu.edu.cn>
2026-03-03 17:14:45 -0800

    0303

--- main.cpp
+++ main.cpp
@@ -2,23 +2,23 @@
 #include <iostream>
 #include <chrono>
 
-int main() {
+int main() 
+{
+    //Base Check
 
+    //Init
     size_t N = 10000000;
-
     Tensor A(N);
     A.fill(1.0f);
-
     auto start = std::chrono::high_resolution_clock::now();
-
     float result = A.sum();
-
     auto end = std::chrono::high_resolution_clock::now();
 
+    //Print
     std::chrono::duration<double> diff = end - start;
-
     std::cout << "Sum: " << result << std::endl;
     std::cout << "Time: " << diff.count() << " seconds\n";
 
+    //Return
     return 0;
 }
