0430c868e90274e0f790cde8b20ad8b8f3fbd8c1
peterzhu <ztj_nwu@snnu.edu.cn>
2026-03-09 16:17:32 -0700

    0309

--- cpp_inference_runtime/graph.cpp
+++ cpp_inference_runtime/graph.cpp
@@ -1,14 +1,15 @@
 #include "graph.h"
 
-void Graph::add_op(Operator* op) {
+void Graph::add_op(Operator* op) 
+{
     ops.push_back(op);
 }
 
-Tensor Graph::run(const Tensor& input) {
-
+Tensor Graph::run(const Tensor& input) 
+{
     Tensor x = input;
-
-    for (auto op : ops) {
+    for (auto op : ops) 
+    {
         x = op->forward(x);
     }
 
--- cpp_inference_runtime/ops.cpp
+++ cpp_inference_runtime/ops.cpp
@@ -16,15 +16,15 @@ Tensor ReLU::forward(const Tensor& input) {
 Tensor Softmax::forward(const Tensor& input) {
 
     Tensor output = input;
-
     float sum = 0.0f;
-
-    for (size_t i = 0; i < input.data.size(); i++) {
+    for (size_t i = 0; i < input.data.size(); i++) 
+    {
         output.data[i] = std::exp(input.data[i]);
         sum += output.data[i];
     }
 
-    for (size_t i = 0; i < input.data.size(); i++) {
+    for (size_t i = 0; i < input.data.size(); i++) 
+    {
         output.data[i] /= sum;
     }
 
