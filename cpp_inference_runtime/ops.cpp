c7fe54073cad8dae7e3766721ce60cb6af34426b
peterzhu <ztj_nwu@snnu.edu.cn>
2026-03-09 16:18:25 -0700

    0309

--- cpp_inference_runtime/ops.cpp
+++ cpp_inference_runtime/ops.cpp
@@ -2,18 +2,21 @@
 #include <algorithm>
 #include <cmath>
 
-Tensor ReLU::forward(const Tensor& input) {
+Tensor ReLU::forward(const Tensor& input) 
+{
 
     Tensor output = input;
-
-    for (size_t i = 0; i < input.data.size(); i++) {
+    for (size_t i = 0; i < input.data.size(); i++) 
+    {
         output.data[i] = std::max(0.0f, input.data[i]);
     }
 
     return output;
 }
 
-Tensor Softmax::forward(const Tensor& input) {
+
+Tensor Softmax::forward(const Tensor& input) 
+{
 
     Tensor output = input;
     float sum = 0.0f;
@@ -29,4 +32,5 @@ Tensor Softmax::forward(const Tensor& input) {
     }
 
     return output;
+
 }
