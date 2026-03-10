e39f6b5ec13cd6bb24cde92ac3d6148609d2946d
peter zhu <ztjnwu@gmail.com>
2026-03-10 00:22:59 +0000

    0309

--- cpp_inference_runtime/ops.h
+++ cpp_inference_runtime/ops.h
@@ -1,17 +1,22 @@
 #pragma once
 #include "tensor.h"
 
-class Operator {
-public:
-    virtual Tensor forward(const Tensor& input) = 0;
+class Operator 
+{
+    public:
+        virtual Tensor forward(const Tensor& input) = 0;
 };
 
-class ReLU : public Operator {
-public:
-    Tensor forward(const Tensor& input) override;
+
+class ReLU : public Operator 
+{
+    public:
+        Tensor forward(const Tensor& input) override;
 };
 
-class Softmax : public Operator {
-public:
-    Tensor forward(const Tensor& input) override;
+
+class Softmax : public Operator 
+{
+    public:
+        Tensor forward(const Tensor& input) override;
 };
